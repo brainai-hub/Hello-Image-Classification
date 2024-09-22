dd5f5dbde4dca6a71c42535d0bcd6e548113a019
Brain AI <73767162+brainai-hub@users.noreply.github.com>
2024-09-22 14:29:44 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,84 +11,16 @@ output_layer_face = compiled_model_face.output(0)
 
 def preprocess(image, input_layer):
     N, input_channels, input_height, input_width = input_layer.shape
-
     resized_image = cv2.resize(image, (input_width, input_height))
     transposed_image = resized_image.transpose(2, 0, 1)
     input_image = np.expand_dims(transposed_image, 0)
-
     return input_image 
-
-def find_faceboxes(image, results, confidence_threshold):
-    results = results.squeeze()
-
-    scores = results[:,2]
-    boxes = results[:, -4:]
-
-    face_boxes = boxes[scores >= confidence_threshold]
-    scores = scores[scores >= confidence_threshold]
-
-    image_h, image_w, image_channels = image.shape 
- 
-    face_boxes = face_boxes*np.array([image_w, image_h, image_w, image_h])
-    face_boxes = face_boxes.astype(np.int64)
-
-    return face_boxes, scores
-
-def draw_age_gender_emotion(face_boxes, image):
-    
-    EMOTION_NAMES =  ['neutral', 'happy', 'sad', 'surprise', 'anger']
-    
-    show_image = image.copy()
-   
-    for i in range(len(face_boxes)):
-
-        xmin, ymin, xmax, ymax = face_boxes[i]
-        face = image[ymin:ymax, xmin:xmax]
-        
-        # --- emotion ---
-        input_image = preprocess(face, input_layer_emo)
-        results_emo = compiled_model_emo([input_image])[output_layer_emo]
- 
-        results_emo = results_emo.squeeze()
-        index = np.argmax(results_emo)       
-        # --- emotion ---
-        
-        
-        # --- age and gender ---
-        input_image_ag = preprocess(face, input_layer_ag)
-        results_ag = compiled_model_ag([input_image_ag]) 
-        age, gender = results_ag[1], results_ag[0]
-        age = np.squeeze(age)
-        age = int(age*100)
-        
-        gender = np.squeeze(gender)
-        
-        if (gender[0]>=0.65):
-            gender = "female"
-            box_color = (200, 200, 0)
-            
-        elif (gender[1]>=0.55):
-            gender = "male"
-            box_color = (0, 200, 200)
-        
-        else:
-            gender = "unknown"
-            box_color = (200, 200, 200)
-        # --- age and gender ---
-         
-        fontScale = image.shape[1]/750
-        
-        text = gender + ' ' + str(age) + ' ' + EMOTION_NAMES[index]
-        cv2.putText(show_image, text, (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 200, 0), 2)
-        cv2.rectangle(img=show_image, pt1=(xmin,ymin), pt2=(xmax,ymax), color=box_color, thickness=2)
-        
-    return show_image
-
 
 def predict_image(image, conf_threshold):
     input_image = preprocess(image, input_layer_face)
-    results = compiled_model_face([input_image])[output_layer_face]
-    face_boxes, scores = find_faceboxes(image, results, conf_threshold)
-    visualize_image = draw_age_gender_emotion(face_boxes, image)
-
-    return visualize_image
+    result_infer = compiled_model([input_image])[output_layer]
+    result_index = np.argmax(result_infer)
+    imagenet_classes = imagenet_filename.read_text().splitlines()
+    imagenet_classes = ["background"] + imagenet_classes
+    imagenet_classes[result_index]
+    return 
